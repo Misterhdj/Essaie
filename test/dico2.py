1e16c709f9af61fd54cf782f4a28b17fe080db99
Juliuslegrand <jules.leuba@eduvaud.ch  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrandrun   git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand/usr/bin/python3  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.chrun   git config --global user.email pv52rdp@eduvaud.chrun  git config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch  git config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch>
2024-02-06 08:21:59 +0100

    tet

--- test/dico2.py
+++ test/dico2.py
@@ -1,35 +1,27 @@
-z=input("Donner le mot à deviner")
-z2=list(z)
-z3=list(z2)
-y = int(input("Nombre de try"))
-pave= list("_"*len(z2))
-print(pave)
-while len(z2)!=0:
-    print("Il reste {0} lettres à deviner".format(len(z2)))
-    
-    x = input("Donnez une lettre")
-    while len(x)>1 or len(x)==0:
-        x=input("Donnez une lettre")
-    if x in z2 :
-       index = []
-       for i in range(len(z3)) :
-           if z3[i] == x :
-               index.append(i)
-       print(index)
-       for i in index :
-            pave[i]=x
-            for i in z2 : 
-               if i == x :
-                z2.remove(x)
-       print(pave)
-       print("Cette lettre est contenue dans le mot")
-       if len(z2)== 0 and y!=0 :
-           print("Tu as gagné")
-           exit()
-    else :
-        print(pave)
-        print("Cette lettre n'est pas contenue dans le mot")
-        y = y-1
-        if y ==0 :
-            print("Tu as perdu") 
-            exit()
+import nltk
+from nltk.corpus import gutenberg
+from collections import Counter
+
+# Chargement du texte français
+nltk.download('punkt')
+nltk.download('stopwords')
+sentences = gutenberg.sents('french.txt')
+
+# Tokenization et mise en minuscules
+words = [word.lower() for sentence in sentences for word in sentence]
+
+# Filtrage des mots vides (stop words)
+stopwords = set(nltk.corpus.stopwords.words('french'))
+words = [word for word in words if word.isalpha() and word not in stopwords]
+
+# Comptage des occurrences des mots
+word_freq = Counter(words)
+
+# Récupération des 5000 mots les plus courants
+most_common_words = [word for word, _ in word_freq.most_common(5000)]
+
+# Écriture des mots dans un fichier texte
+with open("5000_mots_courants_francais.txt", "w", encoding="utf-8") as file:
+    file.write("\n".join(most_common_words))
+
+print("Fichier créé avec succès : 5000_mots_courants_francais.txt")
--- test/test2.py
+++ test/test2.py
@@ -1,10 +1,39 @@
 import tkinter as tk
-
+def option1_function():
+    print("tu as choisi le mode difficle")
 def clic():
     print("Le bouton a été cliqué !")
+def option2_function():
+    print('tu as choisi le mode facile')
 
 test = tk.Tk()
-button = tk.Button(root, text="Cliquez ici", command=clic)
+test.title('le meilleur hangman de tous les temps')
+greeting = tk.Label(test, text="Hello, Tkinter")
+greeting.pack()
+button = tk.Button(test, text="Cliquez ici", command=clic)
 button.pack()
 
+import tkinter as tk
+from tkinter import Menu
+
+# create a menubar
+menu = Menu(test)
+test.config(menu=menu)
+file_menu = Menu(menu, tearoff=False)
+# add a menu item to the menu
+file_menu.add_command(
+    label='Exit',
+    command=test.destroy
+)
+        
+
+# add the File menu to the menubar
+menu.add_cascade(
+    label="File",
+    menu=file_menu,
+    underline=0
+)
+#menu=tk.Menu(menu, tearoff=False)
+#tk.Menu.add_command(label='difficle', command=option1_function) 
+#tk.Menu.add_command(label='facile', command=option2_function)
 test.mainloop()

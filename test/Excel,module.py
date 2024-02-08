9f1728f8fb0f33d94698f30f22f2e1ccd1980fed
GregoLaMenace <pw47nqn@duvaud.ch>
2024-02-08 14:46:40 +0100

    fin

--- test/Excel,module.py
+++ test/Excel,module.py
@@ -1,21 +1,39 @@
 
-from openpyxl import *
+listmot = []
+
 import openpyxl
-workbook = openpyxl.load_workbook('LexiqueNew.xlsx')
-wb = Workbook('LexiqueNew.xlsx')
+nom_fichier_excel = ("LexiqueNew.xlsx")
+nom_feuille_excel = ("fich1")
+numero_colonne_excel = (1)
+def lire_colonne_excel(nom_fichier, nom_feuille, numero_colonne):
+    """
+    Lire les valeurs d'une colonne spécifique dans un fichier Excel.
 
-# grab the active worksheete
-ws = wb.active
+    Args:
+    - nom_fichier (str): Le nom du fichier Excel.
+    - nom_feuille (str): Le nom de la feuille Excel.
+    - numero_colonne (int): Le numéro de la colonne à lire (index de 1).
 
-# Data can be assigned directly to cells
-ws['A1'] = 42
+    Returns:
+    - list: Une liste des valeurs de la colonne spécifiée.
+    """
+    valeurs_colonne = []
 
-# Rows can also be appended
-ws.append([1, 2, 3])
+    # Charger le classeur Excel
+    workbook = openpyxl.load_workbook(nom_fichier)
 
-# Python types will automatically be converted
-import datetime
-ws['A2'] = datetime.datetime.now()
+    try:
+        # Accéder à la feuille
+        sheet = workbook[nom_feuille]
 
-# Save the file
-wb.save("sample.xlsx")
+        # Lire les valeurs de la colonne spécifiée
+        for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=numero_colonne, max_col=numero_colonne):
+            for cell in row:
+                valeurs_colonne.append(cell.value)
+        print(valeurs_colonne[1])
+    finally:
+        # Fermer le classeur Excel
+        workbook.close()
+
+    return valeurs_colonne
+lire_colonne_excel("LexiqueNew.xlsx", "fich1", 1)

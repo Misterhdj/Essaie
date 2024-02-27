48185aefdf6585a205c9cc7a9c81b721ed312ec7
misterhdj <pd05upf@eduvaud.ch>
2024-02-27 09:29:37 +0100

    g

--- test/z.py
+++ test/z.py
@@ -1,1 +1,14 @@
-z=input("Donner le mot à deviner")
+f = open("leaderboard.txt","r")
+lines =f.readlines()
+lead = {}
+i = 0
+for line in lines :
+     line = line.strip("\n")
+     player = line.split(",")
+     i = i + 1 
+     lead[i]=player
+     
+f.close()
+for i in lead :
+     print(lead[i]) 
+

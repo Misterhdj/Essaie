25a80639f799194aa90a6df2b5dc36f7ac1f4c73
Juliuslegrand <jules.leuba@eduvaud.ch  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrandrun   git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand/usr/bin/python3  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.chrun   git config --global user.email pv52rdp@eduvaud.chrun  git config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch  git config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch>
2024-03-19 09:09:31 +0100

    asdfdsf

--- test/test2.py
+++ test/test2.py
@@ -4,17 +4,17 @@ Pseudo=""
 def ordinateur(): #döfinition de l'action executée lorsqu'on appuie sur le bouton ordinateur
     button_ordinateur.destroy() #détruit les deux derniers boutons
     button_joueur.destroy()
-    button_pseudo.place(relx=0.6,rely=0.5,width=50,height=50) #place la nouvelle fenêtre(le contenu)
-    pseudo.place(relx=0.4,rely=0.5,relwidth=0.2,height=50)
-    pseudo_affichage.place(relx=0.475,rely=0.4)
+    #button_pseudo.place(relx=0.6,rely=0.5,width=50,height=50) #place la nouvelle fenêtre(le contenu)
+    #pseudo.place(relx=0.4,rely=0.5,relwidth=0.2,height=50)
+    #pseudo_affichage.place(relx=0.475,rely=0.4)
 
 def joueur(): #définition du bouton joueur, qui permet de faire un 1vs1
     button_ordinateur.destroy() #détruit les deux boutons qui sont devenus inutiles
     button_joueur.destroy()
 def start():#définition du bouton start, on le détruit et on place les deux prochains boutons
     button_s.destroy()
-    button_joueur.place(relx=0.45,rely=0.33,relwidth=0.1,height=50)
-    button_ordinateur.place(relx=0.45,rely=0.66,relwidth=0.1,height=50)
+    #button_joueur.place(relx=0.45,rely=0.33,relwidth=0.1,height=50)
+    #button_ordinateur.place(relx=0.45,rely=0.66,relwidth=0.1,height=50)
 def facile(): #définition du bouton facile, détruit tous les derniers boutons et lance le jeu
     button_f.destroy()
     button_m.destroy()
@@ -43,19 +43,12 @@ def var_pseudo(): #définition de la "FENÊTRE" de pseudo
     global Pseudo
     Pseudo=pseudo.get()
     erreur=tk.Label(window, text='choisi un pseudo!!!!!!') #si l'utilisateur décide de mettre un truc vide
-    if Pseudo == '':                
-        erreur.place(relx=0.475,rely=0.4)
-        pseudo_affichage.destroy()
-        x=1
-    else:
-        pseudo_affichage.destroy()#déstruction des boutons précédants et ffichage des boutons difficultés 
-        pseudo.destroy()
-        erreur.destroy()
-        button_pseudo.destroy()
-        button_f.place(relx=0.45,rely=0.2,relwidth=0.1, height=50)
-        button_m.place(relx=0.45,rely=0.4,relwidth=0.1, height=50)
-        button_h.place(relx=0.45,rely=0.6,relwidth=0.1, height=50)
-        button_c.place(relx=0.45,rely=0.8,relwidth=0.1, height=50)
+    #if Pseudo == '':                
+    #
+        #button_f.place(relx=0.45,rely=0.2,relwidth=0.1, height=50)
+        #button_m.place(relx=0.45,rely=0.4,relwidth=0.1, height=50)
+        #button_h.place(relx=0.45,rely=0.6,relwidth=0.1, height=50)
+        #button_c.place(relx=0.45,rely=0.8,relwidth=0.1, height=50)
     erreur.destroy()
 window = tk.Tk() #création de la fenêtre
 #window.config(bg='magenta')
@@ -64,39 +57,39 @@ window.resizable(0,1)
 window.title('survive or get hanged')
 bienvenue = tk.Label(window, text="bienvenue dans survive or get hanged")
 
-button_q = tk.Button(window, text="quitter le jeu" #création du bouton quitter afin de quitter le jeu à tout moment
+#button_q = tk.Button(window, text="quitter le jeu" #création du bouton quitter afin de quitter le jeu à tout moment
                      , command=window.destroy
                      , fg='red')
-button_q.place(relx=0.9,rely=0,relwidth=0.1, height=50) #place du bouton quitter
+#button_q.place(relx=0.9,rely=0,relwidth=0.1, height=50) #place du bouton quitter
 #button_q.pack(side='left')
-button_s = tk.Button(window #création du bouton start ainsi que son placement 
+#button_s = tk.Button(window #création du bouton start ainsi que son placement 
                      ,text='start'
                      ,command=start
                       ,bg='blue',)
-button_s.place(relx=0.45,rely=0.5,relwidth=0.1, height=50)
+#button_s.place(relx=0.45,rely=0.5,relwidth=0.1, height=50)
 #placements et créations des boutons pour les 4 difficultés
-button_f = tk.Button(window,
+#button_f = tk.Button(window,
                       text='facile',command=facile)
-button_m = tk.Button(window,
+#button_m = tk.Button(window,
                     text='medium',
                      command=medium)
-button_h = tk.Button(window,
+#button_h = tk.Button(window,
                     text='hard',
                      command=hard)
-button_c = tk.Button(window,
+#button_c = tk.Button(window,
                      text='cauchemar',
                      command=cauchemar)
 
-button_ordinateur=tk.Button(window,#création du bouton ordinateur
-                            text='ordinateur',command=ordinateur)
-button_joueur=tk.Button(window,#création du bouton joueur 
-                        text='joueur',command=joueur)
+#button_ordinateur=tk.Button(window,#création du bouton ordinateur
+ #                           text='ordinateur',command=ordinateur)
+#button_joueur=tk.Button(window,#création du bouton joueur 
+  #                      text='joueur',command=joueur)
 
 
-nom=tk.StringVar()
-pseudo_affichage=tk.Label(window,text='choisi ton pseudo')
-pseudo=tk.Entry(window,text= 'choisi ton pseudo',textvariable = nom,)#entrée pour avoir le pseudo que le joueur veut
-button_pseudo=tk.Button(window,text='enter',command=var_pseudo)#bouton qu'on appuie pour avoir le pseudo
+#nom=tk.StringVar()
+#pseudo_affichage=tk.Label(window,text='choisi ton pseudo')
+#pseudo=tk.Entry(window,text= 'choisi ton pseudo',textvariable = nom,)#entrée pour avoir le pseudo que le joueur veut
+#button_pseudo=tk.Button(window,text='enter',command=var_pseudo)#bouton qu'on appuie pour avoir le pseudo
 
 #lamda = 5
 #underscore = tk.Label(window, text =lamda*'_')

7b29bcc52e1e67c13e639737d9c4b3e816029059
Juliuslegrand <jules.leuba@eduvaud.ch  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrandrun   git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand/usr/bin/python3  git config --global user.email jules.leuba@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.ch  git config --global user.name Juliuslegrand  git config --global user.email pv52rdp@eduvaud.chrun   git config --global user.email pv52rdp@eduvaud.chrun  git config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch  git config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.chgit config --global user.email pv52rdp@eduvaud.chgit config --global user.name Juliuslegrandgit config --global user.email pv52rdp@eduvaud.ch>
2024-03-19 08:51:44 +0100

    asdfasdf

--- test/test3.py
+++ test/test3.py
@@ -1,8 +1,5 @@
-import tkinter as tk
-from tkterminal import Terminal
-
-root = tk.Tk()
-terminal = Terminal(pady=5, padx=5)
-terminal.shell = True
-terminal.pack(expand=True, fill='both')
-root.mainloop()
+from pathlib import Path
+script_path = Path(__file__).resolve()
+script_dir = script_path.parent
+path = str(script_dir)+ "/jeudupendufb.jpg"
+f = open(path)
